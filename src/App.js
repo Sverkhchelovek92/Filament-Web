f7f298c67e98f0ce3f25086ccdab8cd0f0f7a18c
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-02-21 12:57:08 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Filament Website</header>
+      <Home />
     </div>
   )
 }
