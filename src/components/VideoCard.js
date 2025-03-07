c1099678be54086dcce1d1a872cddc32775ec1c1
Miroslav Gurnyak <mirmiroslav@gmail.com>
2025-03-07 11:42:19 +0300

    gifs

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
+import { useState } from 'react'
+
 function VideoCard({ video }) {
+  const [isHovered, setIsHovered] = useState(false)
+
   return (
     <div className="video-card">
       <img
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
         className="video-thumbnail"
-        alt=""
-        src={require(`../../public/img/thumbnails/${video.thumbnail}`)}
+        alt={video.title}
+        src={
+          isHovered
+            ? require(`../../public/img/gifs/${video.gif}`)
+            : require(`../../public/img/thumbnails/${video.thumbnail}`)
+        }
       ></img>
 
       <div className="video-info">
